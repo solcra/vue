751f0cf664f336467d9a0b8c779bf1b516642ae8
Solcra <carlosgranadacra@gmail.com>
2017-07-13 22:37:17 -0500

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,6 @@
+export default {
+  appName: 'Solcra',
+  apiKey: '240676942668c0a9be2c3e8f923a1e97',
+  secret: '2260115938893e8dbef25d72e0d26f8e',
+  registeredTo: 'SolcraElPirata'
+}
---
+++ src/components2/Artist.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+    ul
+      h2: a(v-bind:href="artist.url" target="_blank") {{artist.name}}
+      img(:src="artist.image[2]['#text']")
+</template>
+
+<script>
+export default {
+  name:'artist',
+  props:['artist']
+}
+</script>
+
+<style lang="stylus" scoped>
+  li
+    disolay block
+    margin 10px 0
+</style>
